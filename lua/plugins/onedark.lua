ca152a62032b9bbb79b21bbba34b92ac7b2a56a6
Indra <103250258+indraryadi@users.noreply.github.com>
2023-04-21 13:28:35 +0800

    Add files via upload

---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,13 @@
+return {
+  -- add gruvbox
+  -- Configure LazyVim to load gruvbox
+  { "navarasu/onedark.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
